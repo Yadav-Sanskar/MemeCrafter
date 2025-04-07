e2b2ac8f1f3b835bf1dc16d67f6349eefd629350
Sanskar Yadav <yadavsanskar4455@gmail.com>
2025-04-08 02:37:31 +0530

    first commit by-Sanskar Yadva

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer>
+            <p>
+                Created with  by &nbsp;
+                <a className="website-link" href="https://linktr.ee/hmjatt">
+                    Sanskar & Shruti
+                </a>
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
